0d02107f7e186c08f2b9975e9a145ba4ffc3f54c
HANS <delossantoshanschristian@sjcbi.edu.ph>
2025-11-27 01:23:19 +0800

    Update portfolio data and remove admin panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.tsx'
-import AdminLogin from './components/AdminLogin'
-import AdminDashboard from './components/AdminDashboard'
-import ProtectedRoute from './components/ProtectedRoute'
+// import AdminLogin from './components/AdminLogin'
+// import AdminDashboard from './components/AdminDashboard'
+// import ProtectedRoute from './components/ProtectedRoute'
 import { DataProvider } from './contexts/DataContext'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
@@ -16,15 +16,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route 
+            {/* Admin routes disabled */}
+            {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+            {/* <Route 
               path="/admin" 
               element={
                 <ProtectedRoute>
                   <AdminDashboard />
                 </ProtectedRoute>
               } 
-            />
+            /> */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
